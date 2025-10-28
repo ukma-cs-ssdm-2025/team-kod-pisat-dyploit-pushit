a07dadd4d8b2be195ae0075ea3c608a11e4445e1
Ostap Gladun <hladun.ostap@gmail.com>
2025-10-28 23:50:23 +0200

    [REFACTOR] Add boundary test for password length

--- src/api/tests/password-validator.test.js
+++ src/api/tests/password-validator.test.js
@@ -3,3 +3,7 @@ const { isValidPassword } = require('../utils/password-validator.js');
 test('should return false for password shorter than 8 chars', () => {
   expect(isValidPassword('123')).toBe(false);
 });
+
+test('should return true for password equal to 8 chars', () => {
+  expect(isValidPassword('12345678')).toBe(true);
+});
