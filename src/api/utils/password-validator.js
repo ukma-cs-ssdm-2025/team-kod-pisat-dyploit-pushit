5aadf6cd5b85c052961e76f7ff32a759d6fac499
Ostap Gladun <hladun.ostap@gmail.com>
2025-10-28 23:56:15 +0200

    [GREEN] Pass test for missing special char

--- src/api/utils/password-validator.js
+++ src/api/utils/password-validator.js
@@ -1,6 +1,12 @@
 const isValidPassword = (password) => {
   const hasDigit = /\d/;
-  return password.length >= 8 && hasDigit.test(password);
+  const hasSpecial = /[!@#$%^&*]/;
+
+  if (password.length < 8) return false;
+  if (!hasDigit.test(password)) return false;
+  if (!hasSpecial.test(password)) return false;
+
+  return true;
 };
 
 module.exports = { isValidPassword };
