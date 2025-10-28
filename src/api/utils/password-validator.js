69b2a112923a738bcdc1864b3a7ef4ae8fac4630
Ostap Gladun <hladun.ostap@gmail.com>
2025-10-28 23:53:43 +0200

    [REFACTOR] Clean up password validation logic

--- src/api/utils/password-validator.js
+++ src/api/utils/password-validator.js
@@ -1,13 +1,6 @@
 const isValidPassword = (password) => {
   const hasDigit = /\d/;
-
-  if (password.length < 8) {
-    return false;
-  }
-  if (!hasDigit.test(password)) {
-    return false;
-  }
-  return true;
+  return password.length >= 8 && hasDigit.test(password);
 };
 
 module.exports = { isValidPassword };
