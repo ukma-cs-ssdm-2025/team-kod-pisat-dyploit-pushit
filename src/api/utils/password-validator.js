eb3bcf8aa0d804ef894e82a565f48252b9217776
Ostap Gladun <hladun.ostap@gmail.com>
2025-10-28 23:53:16 +0200

    [GREEN] Pass test for missing digit

--- src/api/utils/password-validator.js
+++ src/api/utils/password-validator.js
@@ -1,5 +1,10 @@
 const isValidPassword = (password) => {
+  const hasDigit = /\d/;
+
   if (password.length < 8) {
+    return false;
+  }
+  if (!hasDigit.test(password)) {
     return false;
   }
   return true;
