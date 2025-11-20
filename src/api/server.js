957042fb03a93d3e61f29d3b1cf8c812110c3e5c
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2025-11-20 15:46:46 +0200

    fixing deployment startup

--- src/api/server.js
+++ src/api/server.js
@@ -20,7 +20,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
