f06cd0b8d66677c03b4be771c4940badab78f39b
FeadenGlow <charter.muzyka@gmail.com>
2025-11-26 21:27:38 +0200

    fixing deployment issues

--- src/frontend/src/api.js
+++ src/frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api/v1"
+const API_URL = "http://https://team-kod-pisat-dyploit-pushit-production.up.railway.app/api/v1"
 
 /**
  * Декодує JWT токен для отримання payload (id, username, role)
